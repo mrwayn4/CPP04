a646f2ff311e7ba69f49654a98fd64a10d82865e
Idder Bouram <ibouram@e2r8p13.1337.ma>
2024-11-19 22:09:33 +0100

    [new]

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibouram <ibouram@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 21:52:07 by ibouram           #+#    #+#             */
+/*   Updated: 2024/11/19 22:04:56 by ibouram          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+int	_nbAccounts;
+int	_totalAmount;
+int	_totalNbDeposits;
+int	_totalNbWithdrawals;
+
+Account::Account(int initial_deposit)
+{
+		
+}
+
+Account::~Account()
+{
+	
+}
